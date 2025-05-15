26152ca361b0365d4fee8b1d7866aca329448350
WaiWaiMar347 <waiwaimarsagaing2@gmail.com>
2025-05-15 16:49:27 +0630

    Updated

--- Day5/index.js
+++ Day5/index.js
@@ -1,3 +1,12 @@
 const d=require('./math');
 console.log(d.add(1,2));
 console.log(d.div(4,2));
+
+const fs = require('fs');//import fs from 'fs';
+const path = require('path');//import path from 'path';
+const os=require('os');
+
+fs.readFile('test.txt','utf8',(error,data) => {if(error){throw error} console.log(data)});
+//const full=path.PlactformPath.dirname('test.txt');
+//console.log(full);
+console.log(os.platform()+"  "+os.cpus().length+"  "+os.freemem());
